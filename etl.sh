c916a7d5513c3b75863cbb63f1d6b09f203e17c5
Chien-An, Chen <a19880419@gmail.com>
2022-10-14 15:48:30 +0800

    Update etl.sh

--- etl.sh
+++ etl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t web_event >> /home/ESB16053/logs/web_event_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t web_campaign_impression >> /home/ESB16053/logs/web_campaign_impression_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t web_custno_mapping >> /home/ESB16053/logs/web_custno_mapping_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t web_view >> /home/ESB16053/logs/web_view_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t app_cid_mapping_android >> /home/ESB16053/logs/app_cid_mapping_android_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t app_cid_mapping_ios >> /home/ESB16053/logs/app_cid_mapping_ios_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t app_event_android >> /home/ESB16053/logs/app_event_android_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t app_event_ios >> /home/ESB16053/logs/app_event_ios_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t app_native_android >> /home/ESB16053/logs/app_native_android_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t app_native_ios >> /home/ESB16053/logs/app_native_ios_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t app_custno_mapping_android >> /home/ESB16053/logs/app_custno_mapping_android_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t app_custno_mapping_ios >> /home/ESB16053/logs/app_custno_mapping_ios_$(date +\%Y\%m\%d).log 2>&1
-/usr/bin/python3 /home/ESB16053/chen/ga_etl/etl.py -t aaid_idfa_custno_mapping >> /home/ESB16053/logs/aaid_idfa_custno_mapping_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t web_event >> logs/web_event_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t web_campaign_impression >> logs/web_campaign_impression_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t web_custno_mapping >> logs/web_custno_mapping_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t web_view >> logs/web_view_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t app_cid_mapping_android >> logs/app_cid_mapping_android_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t app_cid_mapping_ios >> logs/app_cid_mapping_ios_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t app_event_android >> logs/app_event_android_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t app_event_ios >> logs/app_event_ios_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t app_native_android >> logs/app_native_android_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t app_native_ios >> logs/app_native_ios_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t app_custno_mapping_android >> logs/app_custno_mapping_android_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t app_custno_mapping_ios >> logs/app_custno_mapping_ios_$(date +\%Y\%m\%d).log 2>&1
+python3 etl.py -t aaid_idfa_custno_mapping >> aaid_idfa_custno_mapping_$(date +\%Y\%m\%d).log 2>&1
